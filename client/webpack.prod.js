74d702ce9009f324e8301269698a77428fd91b43
Pablo Fromer <pablo.fromer@BARMMB207.local>
2021-06-03 22:41:00 -0300

    webpack prod dist config

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,12 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     output: { //common aunque no lo reemplaze
-        path: path.resolve('./', 'dist'),
-        filename: 'bundle.js',
-        publicPath: './'
+        path: path.resolve('./', 'dist')
     },
+    plugins: [new HtmlWebpackPlugin({
+      template: 'index.html'
+    })],
 });
