1544f1a648e92597e8859f20f8de0e853dafa686
Pablo Fromer <pablo.fromer@MacBook-Pro.local>
2021-07-19 21:13:22 -0300

    game won and game lost screens

--- client/src/components/Table/table.js
+++ client/src/components/Table/table.js
@@ -11,19 +11,31 @@ const cardWidth = {
 }
 
 const Table = (props) => {
-  const { gameStarted, allCards, oponentPlayerName, currentPlayerName } = props;
+  const { gameStarted, allCards, oponentPlayerName, currentPlayerName, gameWon, gameLost } = props;
+
+  console.log("GAME WON", gameWon)
+  console.log("GAME LOST", gameLost)
+  console.log("GAME STARTED", gameStarted)
+
 
 
   return (
     <React.Fragment>
-      {gameStarted && (
-        <Container gameOver={false}>
+      {gameStarted &&(
+        <Container gameOver={gameLost || gameWon}>
           <OponentPlayerLabel>{oponentPlayerName}</OponentPlayerLabel>
           <MainPlayerLabel>{currentPlayerName}</MainPlayerLabel>
           {allCards.map(card =>
             <Card card={card} key={card.id} clickable={true} cardWidth={cardWidth.currentPlayer}></Card>
           )}
         </Container>
+      )}
+      {(gameLost || gameWon) && (
+        <FlexContainer>
+          {gameLost && (<GameOver>You Lost <span>😬</span></GameOver>)}
+          {gameWon && (<GameOver>You Won!!! Congratulations!!! <span>😎😎😎</span></GameOver>)}
+          <Action href={process.env.DOMAIN}>Keep Playing!</Action>
+        </FlexContainer>
       )}
     </React.Fragment>
   );
@@ -34,6 +46,8 @@ const mapStateToProps = state => ({
   gameStarted: state.gameReducer.started,
   currentPlayerName: state.gameReducer.players.filter(p => p.isCurrentPlayer)[0].name,
   oponentPlayerName: state.gameReducer.players.filter(p => !p.isCurrentPlayer)[0].name,
+  gameWon: state.gameReducer.gameWon,
+  gameLost: state.gameReducer.gameLost
 });
 
 export default connect(mapStateToProps)(Table);
